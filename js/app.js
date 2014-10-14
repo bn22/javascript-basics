98ebcc8eed61bccb3bb10a4f21585754c11d5edb
Bruce Ng <bn22@uw.edu>
2014-10-14 12:11:49 -0700

    End of Class

--- js/app.js
+++ js/app.js
@@ -3,30 +3,52 @@
 
 var name = 'Dave';
 
+var fruits = ['apple', 'banana', 'pear', 'kiwi'];
 
-function sayHello(name) {
-    alert('Hello ' + name);
+fruits.push('mango');
+
+for (var idx = 0; idx < fruits.length; idx++) {
+    console.log(fruits[idx]);
 }
 
-var f = sayHello;
+//function sayHello(name) {
+  //  alert('Hello ' + name);
+//}
+
+//var f = sayHello;
 
 //sayHello('Fred');
 //f('Fred');
 
-function onNameChange() {
-    var nameInput = document.getElementById('name-input');
-    document.getElementById('message').innerHTML = 'Hello  ' + nameInput.value;
-}
+//function onNameChange() {
+  //  var nameInput = document.getElementById('name-input');
+   // document.getElementById('message').innerHTML = 'Hello  ' + nameInput.value;
+//}
 
 function onReady() {
     var elem = document.getElementById('message');
     elem.innerHTML = 'Hello ' + name;
 
-    var nameInput = document.getElementById('name-input')
-    nameInput.value = 'Username';
-
+    var nameInput = document.getElementById('name-input');
     nameInput.value = name;
-    nameInput.addEventListener('keyup', onNameChange);
+
+    nameInput.addEventListener('keyup', function() {
+        elem.innerHTML = 'Hello ' + nameInput.value;
+    });
+
+    var fruitsList = document.getElementById('fruits-list');
+    fruits.forEach(function(fruit) {
+        var fruitLi;
+        fruitLi = document.createElement('li');
+        fruitLi.innerHTML = fruit;
+        fruitsList.appendChild(fruitLi);
+    });
+
+    var animalPic = document.getElementById('animal-pic');
+    animalPic.addEventListener('click', function() {
+       var animalSound = document.getElementById('animal-sound');
+       animalSound.play();
+    });
 }
 
 document.addEventListener('DOMContentLoaded', onReady);
